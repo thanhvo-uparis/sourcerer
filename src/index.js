8ecdadb2e03e17a0c5b8cc187c6057a95e283dcd
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-10-31 22:42:35 +0100

    etape1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import UserInfos from './components/UserInfos/UserInfos';
+import Query from './query/Query';
+import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-ReactDOM.render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: { authorization: "bearer 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"},
+  cache: new InMemoryCache()
+});
+
+const GET_LOGIN = gql`
+    query { 
+        viewer { 
+            login
+            avatarUrl
+            followers(first: 1){
+              totalCount
+            }
+            location
+            company
+            repositories(first: 1){
+              totalCount
+            }
+            following(first: 1){
+              totalCount
+            }
+          }
+    }
+`;
+
+function ExchangeRates() {
+
+  const { loading, error, data } = useQuery(GET_LOGIN);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+    return(
+      <div>
+        <img src={data.viewer.avatarUrl} />
+        <p>{data.viewer.login}</p>
+          <p> {data.viewer.following.totalCount} </p>
+          <div>
+            <p> Following: {data.viewer.following.totalCount} </p>
+          </div>
+
+      </div>
+    )
+}
+
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates/>
+    </div>
+    
+  );
+}
+
+render(
+
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
