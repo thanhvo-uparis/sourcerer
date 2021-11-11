ec348b32f7f0d29962e7247e93ed6e81f52d7b26
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-11-11 23:02:36 +0100

    clear app

--- src/index.js
+++ src/index.js
@@ -1,75 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Schema from './schema/schema';
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
+  ApolloProvider
 } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
-  headers: { authorization: "bearer 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"},
+  headers: { authorization: "bearer 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"},
   cache: new InMemoryCache()
 });
 
-const GET_LOGIN = gql`
-    query { 
-        viewer { 
-            login
-            avatarUrl
-            followers(first: 1){
-              totalCount
-            }
-            location
-            company
-            repositories(first: 1){
-              totalCount
-            }
-            following(first: 1){
-              totalCount
-            }
-          }
-    }
-`;
-
-function ExchangeRates() {
-
-  const { loading, error, data } = useQuery(GET_LOGIN);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-    return(
-      <div>
-        <img src={data.viewer.avatarUrl} />
-        <p>{data.viewer.login}</p>
-          <p> {data.viewer.following.totalCount} </p>
-          <div>
-            <p> Following: {data.viewer.following.totalCount} </p>
-          </div>
-
-      </div>
-    )
-}
-
-
-function App() {
-  return (
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <ExchangeRates/>
-    </div>
-    
-  );
-}
-
-render(
-
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+ReactDOM.render(
+  <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>,
+  </React.StrictMode>,
   document.getElementById('root'),
 );
